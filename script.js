84ef401e5d8c3a790621d630642504af2a4ad0e3
Shawn Wright <shawncodewright@gmail.com>
2023-03-16 10:34:47 -0500

    Added comments for how regex works for code

--- script.js
+++ script.js
@@ -4,6 +4,21 @@
 
 function telephoneCheck(phoneNumber) {
   // regular expression to match valid US phone numbers
+  /* This function uses a regular expression to check if the input string matches the format 
+  of a valid US phone number. Here's how the regular expression works:
+  
+  `^`: start of string
+`(1\s?)?`: optional country code (1) followed by an optional space character
+`(\(\d{3}\)|\d{3})`: either a three-digit area code enclosed in parentheses or just a three-digit area code
+`[- ]?`: an optional dash or space character
+`\d{3}`: three digits for the exchange code
+`[- ]?`: an optional dash or space character
+`\d{4}`: four digits for the subscriber number
+`$`: end of string 
+
+This regular expression matches all valid US phone number formats listed in the prompt,
+ as well as variations such as spaces and dashes in different places.
+*/
   const regex = /^(1\s?)?(\(\d{3}\)|\d{3})[- ]?\d{3}[- ]?\d{4}$/;
   return regex.test(phoneNumber);
 }
